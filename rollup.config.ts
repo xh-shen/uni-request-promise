7bc85f0c425aaf0ca5c09ecd1711733235a6d189
shenqz <9509394@qq.com>
2020-12-30 14:12:32 +0800

    添加发布文件及发布脚本

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@
  * @Author: shen
  * @Date: 2020-12-28 10:05:11
  * @LastEditors: shen
- * @LastEditTime: 2020-12-29 08:17:11
+ * @LastEditTime: 2020-12-30 14:03:54
  * @Description:
  */
 
@@ -17,8 +17,8 @@ const pkg = require('./package.json')
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'es', sourcemap: true },
-    { file: 'dist/uni-request.umd.js', name: 'uniRequest', format: 'umd', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.main, name: 'uniRequest', format: 'umd', sourcemap: true },
   ],
   external: [],
   watch: {
---
+++ script/publish.sh
@@ -0,0 +1,29 @@
+###
+ # @Author: shen
+ # @Date: 2020-12-30 14:10:08
+ # @LastEditors: shen
+ # @LastEditTime: 2020-12-30 14:11:58
+ # @Description:
+###
+
+rm -rf ./dist
+rm -rf ./lib
+rm -rf ./types
+yarn run build
+
+old_registry=$(npm config get registry)
+npm config set registry https://registry.npmjs.org
+whoami=$(npm whoami 2>/dev/null)
+
+if [ -z "$whoami" ]
+then
+   echo "login plz..."
+   npm login
+fi
+echo "I am: $(npm whoami)"
+
+sleep 1
+echo "Begin publish..."
+npm publish --access=public "$@"
+
+npm config set registry ${old_registry}
